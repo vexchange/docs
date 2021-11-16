823b121d04d08203c77ddb98b51a35b920e0b138
Anderson Aguiar <revsoft10@gmail.com>
2021-11-16 15:33:12 +1100

    Cleanup

--- src/pages/404.js
+++ src/pages/404.js
@@ -9,7 +9,7 @@ const NotFoundPage = props => (
     <BG />
     <SEO title="404: Not found" path={props.location.pathname} />
     <div style={{maxWidth: 1440, padding: 32,margin: 'auto'}}>
-      <h1>Missing Unicorn 🦄</h1>
+      <h1>Page not found</h1>
       <p>You just found a page that doesn&#39;t exist.</p>
     </div>
   </Layout>
