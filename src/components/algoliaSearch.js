9bd49e3e94d5a28085d5d7bd5b181d5d28dcfbad
Anderson Aguiar <revsoft10@gmail.com>
2021-11-16 15:54:38 +1100

    Hide search

--- src/components/algoliaSearch.js
+++ src/components/algoliaSearch.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import styled from 'styled-components'
 
 const SearchWrapper = styled.div`
+  display: none;
   position: relative;
   width: 100%;
   form {
